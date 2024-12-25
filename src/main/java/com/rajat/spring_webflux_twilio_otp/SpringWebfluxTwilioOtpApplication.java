4f6b3c7548b51aa6dda303d877b6e4b7df74f688
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-12-25 13:52:54 +0530

    Init project

---
+++ src/main/java/com/rajat/spring_webflux_twilio_otp/SpringWebfluxTwilioOtpApplication.java
@@ -0,0 +1,13 @@
+package com.rajat.spring_webflux_twilio_otp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxTwilioOtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxTwilioOtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rajat/spring_webflux_twilio_otp/SpringWebfluxTwilioOtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajat.spring_webflux_twilio_otp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxTwilioOtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
