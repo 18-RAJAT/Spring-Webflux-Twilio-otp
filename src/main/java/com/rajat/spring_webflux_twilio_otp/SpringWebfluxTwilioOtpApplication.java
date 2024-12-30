f8c933ccb4f912213f9826b5d828ab5dcbcfc7f1
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-12-30 21:34:06 +0530

    Minor changes and add dependencies

--- src/main/java/com/rajat/spring_webflux_twilio_otp/SpringWebfluxTwilioOtpApplication.java
+++ src/main/java/com/rajat/spring_webflux_twilio_otp/SpringWebfluxTwilioOtpApplication.java
@@ -1,25 +1,14 @@
 package com.rajat.spring_webflux_twilio_otp;
 
-import com.twilio.Twilio;
-import com.rajat.spring_webflux_twilio_otp.config.TwilioConfig;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class SpringWebfluxTwilioOtpApplication {
 
-
-	@Autowired
-	private TwilioConfig twilioConfig;
-	@PostConstruct
-	public void initTwilio()
-	{
-		Twilio.init(twilioConfig.getAccountSid(),twilioConfig.getAuthToken());
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebfluxTwilioOtpApplication.class, args);
 	}
-
 }
