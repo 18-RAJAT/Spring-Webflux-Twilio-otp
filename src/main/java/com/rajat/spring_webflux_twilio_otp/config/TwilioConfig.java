701ef5317a0b61af9e05049dd76ba9dd1d42e5ec
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-12-30 21:31:00 +0530

    Twilio-Account-configuration

--- src/main/java/com/rajat/spring_webflux_twilio_otp/config/TwilioConfig.java
+++ src/main/java/com/rajat/spring_webflux_twilio_otp/config/TwilioConfig.java
@@ -1,14 +1,28 @@
 package com.rajat.spring_webflux_twilio_otp.config;
 
-import lombok.Data;//getter-setter
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import com.twilio.Twilio;
 
 @Configuration
-@ConfigurationProperties(prefix = "twilio")
-@Data
 public class TwilioConfig {
+
+    @Value("${twilio.account_sid}")
     private String accountSid;
+
+    @Value("${twilio.auth_token}")
     private String authToken;
+
+    @Value("${twilio.trial_number}")
     private String trialNumber;
+
+    @PostConstruct
+    public void init() {
+        Twilio.init(accountSid, authToken);
+    }
+
+    public String getTrialNumber() {
+        return trialNumber;
+    }
 }
